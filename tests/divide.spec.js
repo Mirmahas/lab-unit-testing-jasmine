7ea58c8b870fc70eab2878aee9e59f0d8d6aa807
Mirmahas <morenako361@gmail.com>
2024-06-21 22:55:12 +0200

    Solved lab

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,14 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+describe("funcion divide", () => {
+  if (
+    ("Should be defind",
+    () => {
+      expect(divide).toBeDefined();
+    })
+  );
+  describe("Function - divide", () => {
+    // Each `it` block represents 1 test. You can use the following as a template:
+    it("", () => {});
+  });
+});
